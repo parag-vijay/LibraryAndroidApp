5449aad6f05adfd15f4e39861bd0ba67edc3bbb9
Parag Vijayvergia <parag.vijayvergia@sjsu.edu>
2017-12-04 14:52:00 -0800

    Creating model for Books and User

---
+++ app/src/main/java/com/android/cmpe277project/model/Book.java
@@ -0,0 +1,120 @@
+package com.android.cmpe277project.model;
+
+import java.util.List;
+
+/**
+ * Created by parag on 12/4/17.
+ */
+
+public class Book {
+    private String id;
+    private String author;
+    private String title;
+    private String callNumber;
+    private String publisher;
+    private int yearOfPub;
+    private String location;
+    private int copies;
+
+    private enum Status{
+        AVAILABLE,
+        CHECKED_OUT,
+        WAITING;
+    }
+
+    private Status status;
+
+    private List<String> keywords;
+    private String image;
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCallNumber() {
+        return callNumber;
+    }
+
+    public void setCallNumber(String callNumber) {
+        this.callNumber = callNumber;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public int getYearOfPub() {
+        return yearOfPub;
+    }
+
+    public void setYearOfPub(int yearOfPub) {
+        this.yearOfPub = yearOfPub;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public int getCopies() {
+        return copies;
+    }
+
+    public void setCopies(int copies) {
+        this.copies = copies;
+    }
+
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public List<String> getKeywords() {
+        return keywords;
+    }
+
+    public void setKeywords(List<String> keywords) {
+        this.keywords = keywords;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
+
+
---
+++ app/src/main/java/com/android/cmpe277project/model/User.java
@@ -0,0 +1,71 @@
+package com.android.cmpe277project.model;
+
+import java.util.List;
+
+/**
+ * Created by parag on 12/4/17.
+ */
+
+public class User {
+    private String name;
+    private String email;
+    private String token; // ?
+    private int universityId;
+    private List<Book> bookList;
+
+
+    private enum UserType{
+        LIBRARIAN,
+        PATRON;
+    }
+
+    private UserType userType;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public int getUniversityId() {
+        return universityId;
+    }
+
+    public void setUniversityId(int universityId) {
+        this.universityId = universityId;
+    }
+
+    public List<Book> getBookList() {
+        return bookList;
+    }
+
+    public void setBookList(List<Book> bookList) {
+        this.bookList = bookList;
+    }
+
+    public UserType getUserType() {
+        return userType;
+    }
+
+    public void setUserType(UserType userType) {
+        this.userType = userType;
+    }
+}
