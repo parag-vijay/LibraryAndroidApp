e426106e1d1a06b07ccd5a3daa85e45ccbad5221
Parag Vijayvergia <parag.vijayvergia@sjsu.edu>
2017-12-04 15:02:55 -0800

    Creating Librarian Presenter

---
+++ app/src/main/java/com/android/cmpe277project/service/librarian/LibrarianPresenter.java
@@ -0,0 +1,13 @@
+package com.android.cmpe277project.service.librarian;
+
+import com.android.cmpe277project.base.Presenter;
+import com.android.cmpe277project.base.ViewInteractor;
+
+/**
+ * Created by parag on 12/4/17.
+ */
+
+public interface LibrarianPresenter extends Presenter<ViewInteractor> {
+
+
+}
