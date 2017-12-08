57ab5295343affcf86e29dd670ef5e3e7c854b42
Aaditya Deowanshi <aadityadeowanshi@hotmail.com>
2017-12-07 16:41:54 -0800

    Change name and icon

--- android/app/src/main/java/com/android/cmpe277project/module/auth/OnBoardActivity.java
+++ android/app/src/main/java/com/android/cmpe277project/module/auth/OnBoardActivity.java
@@ -47,12 +47,12 @@ public class OnBoardActivity extends BaseActivity implements AuthViewInteractor{
 
         user = userPreference.readUser();
 
-        Bundle b = new Bundle();
+        /*Bundle b = new Bundle();
         b.putString("type", "patron");
         startActivity(DashBoardActivity.class, b);
         finish();
-        return;
-        /*if( user != null) {
+        return;*/
+        if( user != null) {
             Bundle b = new Bundle();
 
             if (user.getEmail().contains("sjsu.edu"))
@@ -68,7 +68,7 @@ public class OnBoardActivity extends BaseActivity implements AuthViewInteractor{
         authPresenter = new AuthPresenterImpl();
         authPresenter.attachViewInteractor(this);
 
-        loadFragments();*/
+        loadFragments();
     }
 
     @OnPageChange(R.id.home_viewpager)
